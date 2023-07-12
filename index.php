4a0996976e1f8537ded5eb8d2128539f38f2db56
Gianluca-Basili <gianluc.basili@gmail.com>
2023-07-12 14:31:26 +0200

    add function get and set

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+    require_once __DIR__.'/models/sistemiComunicazione.php';
+
+    $comm = new SistemiComunicazione('Gianluca', 'Francesco','Saluto', 'ciao francesco');
+    
+    echo "Mittente:". $comm->getMittente()."<br/>";
+    echo "Destinatario:".$comm->getDestinatario()."<br/>";
+    echo "Oggetto:".$comm->getOggetto()."<br/>";
+    echo "Contenuto:".$comm->getContenuto()."<br/>";
+    echo "Stato invio:".$comm->invio()."<br/>";
+
 
 ?>
